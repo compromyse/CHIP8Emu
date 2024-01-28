5080c1c72f445547c77ca4a53e631303d4c28d3f
Raghuram Subramani <raghus2247@gmail.com>
2024-01-28 11:36:09 +0530

    add nix development environment

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
+    in {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cmake
+          SDL2
+        ];
+        shellHook = ''
+          tmux
+          exit
+        '';
+      };
+    };
+}
