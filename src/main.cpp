b82ddcd666847a9e4eae4c79c63d84d9fb3bf903
Raghuram Subramani <raghus2247@gmail.com>
2024-02-08 10:58:45 +0530

    use cycle delay and arguments

--- src/main.cpp
+++ src/main.cpp
@@ -26,11 +26,11 @@ void createPlatform(int videoScale) {
   );
 }
 
-void mainLoop(int videoScale) {
-    platform->Update(chip8->video, videoScale);
+void mainLoop(void* videoScale) {
+    platform->Update(chip8->video, *(int*)videoScale);
 }
 
-void update() {
+void update(int cycleDelay) {
   while (true) {
     quit = platform->ProcessInput(&chip8->keypad);
 
@@ -38,18 +38,12 @@ void update() {
     if (quit) emscripten_cancel_main_loop();
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
 #else
-    usleep(2 * 1000);
+    usleep(cycleDelay * 1000);
 #endif
     chip8->Cycle();
   }
 }
 
-#ifdef __EMSCRIPTEN__
-int main() {
-  int videoScale = 10;
-  int cycleDelay = 0;
-  char const* filename = "roms/rom.ch8";
-#else
 int main(int argc, char** argv) {
   if (argc != 4) {
     std::cerr << "Usage: " << argv[0] << " <Scale> <Delay> <ROM>" << std::endl;
@@ -59,7 +53,6 @@ int main(int argc, char** argv) {
   int videoScale = std::stoi(argv[1]);
   int cycleDelay = std::stoi(argv[2]);
   char const* filename = argv[3];
-#endif
 
   Table* table = new Table(chip8);
 
@@ -69,12 +62,12 @@ int main(int argc, char** argv) {
   createPlatform(videoScale);
 
   std::thread timerThread(&Chip8::TimerUpdateThread, chip8, platform);
-  std::thread updateThread(update);
+  std::thread updateThread(update, cycleDelay);
 #ifdef __EMSCRIPTEN__
-  emscripten_set_main_loop(mainLoop, 0, 1);
+  emscripten_set_main_loop_arg(mainLoop, (void*) &videoScale, 0, 1);
 #else
   while (!quit)
-    mainLoop(videoScale);
+    mainLoop((void*) &videoScale);
   timerThread.detach();
 #endif
 
